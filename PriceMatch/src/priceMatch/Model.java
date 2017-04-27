89069afd65165db95bd9b5e8b649cf2b403d3847
BearBurg <ruqing@seas.upenn.edu>
2017-04-27 15:58:38 -0400

    S1

---
+++ PriceMatch/src/priceMatch/Model.java
@@ -0,0 +1,18 @@
+package priceMatch;
+
+
+public class Model {
+	String keyword;
+	String brand;
+
+	public Model(String keyword, String brand) {
+		super();
+		this.keyword = keyword;
+		this.brand = brand;
+	}
+	
+	public String result(){
+		
+	};
+
+}
